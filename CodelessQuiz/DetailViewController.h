63544c55501d6c64c0e4cb3541bc15d1cbe93d44
Joe <JoeBlaze2ooX@gmail.com>
2012-04-24 15:06:39 -0400

    Initial Commit

---
+++ CodelessQuiz/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  CodelessQuiz
+//
+//  Created by  on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
---
+++ CodelessQuiz/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  CodelessQuiz
+//
+//  Created by  on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
